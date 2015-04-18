c5afd0c15430463627775bcde89d987451fc9acf
Vagrant User <vagrant@packer-virtualbox-iso-1422588891.vagrantup.com>
2015-04-18 13:51:25 +0000

    Add routing for all categories.

--- src/CategoriesHandler.php
+++ src/CategoriesHandler.php
@@ -2,8 +2,6 @@
 
 namespace BaunPlugin\Categories;
 
-use Categories;
-
 /**
  * Class CategoriesHandler
  *
@@ -11,30 +9,37 @@ use Categories;
  */
 class CategoriesHandler
 {
-    /**
-     * @var array all blog posts
-     */
-    protected $allPosts;
+    private $config;
+    private $router;
+    private $events;
+    private $theme;
 
     /**
-     * @param $allPosts
+     * @param $config
+     * @param $router
+     * @param $events
+     * @param $theme
      */
-    public function __construct($allPosts) {
-        if (!is_array($allPosts)) {
-            $this->allPosts = array();
-        } else {
-            $this->allPosts = $allPosts;
-        }
+    public function __construct($config, $router, $events, $theme) {
+        $this->config = $config;
+        $this->router = $router;
+        $this->events = $events;
+        $this->theme = $theme;
     }
 
     /**
-     * @return array of category names
+     * @param $posts array posts to check
+     * @return array of category names belonging to the given posts
      */
-    public function findCategories()
+    public function findCategories($posts)
     {
         $categories = array();
 
-        foreach ($this->allPosts as $post) {
+        if (!$posts) {
+            return array();
+        }
+
+        foreach ($posts as $post) {
             if (!isset($post['info']['categories']) || !$post['info']['categories']) {
                 continue;
             }
@@ -42,12 +47,84 @@ class CategoriesHandler
             foreach (explode(',', $post['info']['categories']) as $categoryName) {
                 $categoryName = trim($categoryName);
 
-                if (!in_array($categoryName, $categories)) {
+                if (in_array($categoryName, $categories) === false) {
                     $categories[] = $categoryName;
                 }
             }
         }
 
         return $categories;
+    }
+
+    /**
+     * Add routes that correspond to blog categories
+     *
+     * @param $allCategories
+     * @param $allPosts
+     */
+    public function addCategoryRoutes($allCategories, $allPosts)
+    {
+        foreach ($allCategories as $category) {
+            $this->router->add('GET', $this->getPath($category), function () use ($category, $allPosts) {
+                $page = isset($_GET['page']) && $_GET['page'] ? abs(intval($_GET['page'])) : 1;
+
+                $categoryPosts = array();
+                foreach ($allPosts as $post) {
+                    if (in_array($category, $this->findCategories(array($post))) !== false) {
+                        $categoryPosts[] = $post;
+                    }
+                }
+
+                $postsPagination = [
+                    'total_pages'  => $this->countPages($categoryPosts),
+                    'current_page' => $page,
+                    'base_url'     => $this->getPath($category)
+                ];
+
+                $this->events->emit('baun.beforeBlogRender', $categoryPosts, $postsPagination);
+
+                return $this->theme->render('blog', [
+                    'all_posts'  => $categoryPosts,
+                    'posts'      => $this->getPostsForPage($categoryPosts, $page),
+                    'pagination' => $postsPagination,
+                ]);
+            });
+        }
+    }
+
+    private function getPath($category)
+    {
+        return $this->config->get('plugins-maccath-baun-categories-categories.category_url') . $category;
+    }
+
+    private function getPostsForPage($posts, $page)
+    {
+        if (!$posts) {
+            return array();
+        }
+
+        $postsPerPage = $this->config->get('blog.posts_per_page');
+        $totalPages = $this->countPages($posts);
+
+        if ($page > $totalPages) {
+            $page = $totalPages;
+        }
+
+        $chunk = !$page || $page <= 1 ? 0 : $page - 1;
+
+        $pages = array_chunk($posts, $postsPerPage);
+
+        return $pages[$chunk];
+    }
+
+    private function countPages($posts)
+    {
+        if (!$posts) {
+            return 0;
+        }
+
+        $postsPerPage = $this->config->get('blog.posts_per_page');
+
+        return count(array_chunk($posts, $postsPerPage));
     }
 }
