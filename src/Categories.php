cadcc1e430e2a208097b4888cd75578168c8619f
Vagrant User <vagrant@packer-virtualbox-iso-1422588891.vagrantup.com>
2015-04-18 12:12:35 +0000

    Add CategoryHandler to find all existing categories.

--- src/Categories.php
+++ src/Categories.php
@@ -2,12 +2,25 @@
 
 use Baun\Plugin;
 
+/**
+ * Class Categories
+ *
+ * @package BaunPlugin\Categories
+ */
 class Categories extends Plugin
 {
+    /**
+     * Initialise Categories plugin, adding appropriate event listeners
+     */
     public function init()
     {
         $this->events->addListener('baun.afterGlobals', function($event, $theme) {
             $theme->addGlobal('category_url', $this->config->get('categories.category_url'));
+        });
+
+        $this->events->addListener('baun.filesToPosts', function($event, $allPosts) {
+            $categoriesSetup = new CategoriesHandler($allPosts);
+            $this->events->emit('baun.categoriesFound', $categoriesSetup->findCategories());
         });
     }
 }
