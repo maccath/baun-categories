9cc2e89544edaa5a42dd414dbf48cb54d803548a
Katy Ereira <kachii@gmail.com>
2015-04-19 16:24:01 +0100

    Remove trailing slash from categories url

--- config/categories.php
+++ config/categories.php
@@ -5,7 +5,7 @@ return [
     // The title of the categories page
     'title' => 'Categories',
     // The URL used to access the categories
-    'category_url' => 'categories/',
+    'category_url' => 'categories',
     // Exclude category list page from nav
     'exclude_from_nav' => false,
 
